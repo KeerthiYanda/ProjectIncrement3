f18fb181dbf2e3e002236218b5add4d2de879ed3
KeerthiYanda <keethu.yanda@gmail.com>
2015-07-27 23:37:19 -0500

    ProjectIncr3

---
+++ GCDAsyncSocket.h
@@ -0,0 +1,22 @@
+//
+//  GCDAsyncSocket.h
+//  HelloRMCore
+//
+//  Created by Ballu, Sandeep (UMKC-Student) on 6/29/15.
+//  Copyright (c) 2015 Romotive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Security/Security.h>
+#import <Security/SecureTransport.h>
+#import <dispatch/dispatch.h>
+#import <Availability.h>
+
+#include <sys/socket.h> // AF_INET, AF_INET6
+
+
+#ifndef HelloRMCore_GCDAsyncSocket_h
+#define HelloRMCore_GCDAsyncSocket_h
+
+
+#endif
